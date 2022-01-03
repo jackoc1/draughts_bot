f6d1d49d17036bc17a70e1bbd1cc712d14ba32a9
jackoc1 <jackaoconnor1@gmail.com>
2022-01-03 21:32:58 +0000

    static valid moves method added.
    likely redundant one liner method signatures removed since should just call the relevant objects relevant method directly

--- draughts/draughts.py
+++ draughts/draughts.py
@@ -1,4 +1,4 @@
-from draughts.board import Board, Piece
+from draughts.board import Board
 
 
 class Draughts:
@@ -13,28 +13,20 @@ class Draughts:
     player_1 = property(lambda self: self._player_1)
     player_2 = property(lambda self: self._player_2)
     active_player = property(lambda self: self._active_player)
-    turncount = property(lambda self: self._turn_count)
+    turn_count = property(lambda self: self._turn_count)
 
     def play(self):
-        pass
-
-    def valid_moves(self, player):  # player 1 == 0, player 2 colour == 1
-        pass
-
-    def get_player_move(self):  # have if capturing check
         pass
 
     def next_turn(self):
         pass
 
-    def _valid_moves(self, player):
+    @staticmethod
+    def valid_moves(player, board):
+        pass
+
+    def _valid_moves(self, player):  # player 1 == 0, player 2 == 1
         pass
 
     def _valid_captures(self, player):
-        pass
-
-    def _move(self, piece, position):  # likely will delete
-        pass
-
-    def _capture(self, piece, position):  # likely will delete
         pass
