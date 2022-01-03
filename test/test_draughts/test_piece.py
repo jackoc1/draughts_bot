378ea48066d374eea8e77db95faf03118b072dd1
jackoc1 <jackaoconnor1@gmail.com>
2022-01-03 11:40:40 +0000

    Piece unit tests written

--- test/test_draughts/test_piece.py
+++ test/test_draughts/test_piece.py
@@ -2,9 +2,33 @@ import unittest
 from draughts.piece import Piece
 
 
-class TestPiece(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class PieceTest(unittest.TestCase):
+    def test_no_playerless_instantiation(self):
+        self.assertRaises(Exception, Piece.__init__, ())
+
+    def test_player_must_be_0_or_1(self):
+        Piece(0)
+        Piece(1)
+        self.assertRaises(Exception, Piece.__init__, 2)
+        self.assertRaises(Exception, Piece.__init__, "dark")
+
+    def test_default_promoted_status_false(self):
+        piece = Piece(0)
+        self.assertEqual(piece.promoted, False)
+
+    def test_promote_unpromoted_piece(self):
+        piece = Piece(1)
+        self.assertEqual(piece.promoted, False)
+        piece.promote()
+        self.assertEqual(piece.promoted, True)
+
+    def test_promote_promoted_piece(self):
+        piece = Piece(0)
+        self.assertEqual(piece.promoted, False)
+        piece.promote()
+        self.assertEqual(piece.promoted, True)
+        piece.promoted()
+        self.assertEqual(piece.promoted, True)
 
 
 if __name__ == '__main__':
