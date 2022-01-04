4ebef96a18d1f50daee106e7aa787e6dc40a79d5
jackoc1 <jackaoconnor1@gmail.com>
2022-01-04 00:56:00 +0000

    move class added so not working with raw lists

---
+++ test/test_draughts/test_move.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class MoveTest(unittest.TestCase):
+    def test_value_error_if_start_position_not_valid(self):
+        pass
+
+    def test_value_error_if_end_position_not_valid(self):
+        pass
+
+    def test_value_error_if_both_positions_not_valid(self):
+        pass
+
+    def test_no_error_if_both_positions_valid(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
