c7fe3a7f7ee9bbc238b9c9d9b96c6e389d2db3cd
jackoc1 <jackaoconnor1@gmail.com>
2022-01-06 20:43:30 +0000

    order is now (row, column)

--- test/test_draughts/test_capture.py
+++ test/test_draughts/test_capture.py
@@ -12,35 +12,35 @@ class CaptureTest(unittest.TestCase):
 
     def test_value_error_if_end_position_not_on_board(self):
         self.assertRaises(ValueError, Capture.__init__, (self.piece, (7, 7), (9, 9)))
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (0, 2), (-2, 4)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (2, 0), (4, -2)))
         self.piece.promote()
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (0, 0), (2, -2)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (0, 0), (-2, 2)))
 
     def test_value_error_if_unpromoted_piece_not_finishing_forward_two_squares_diagonally(self):
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (3, 5), (1, 3)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (5, 3), (3, 1)))
         self.assertRaises(ValueError, Capture.__init__, (self.piece, (4, 4), (7, 7)))
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (5, 5), (7, 3)))
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (3, 3), (3, 5)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (5, 5), (3, 7)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (3, 3), (5, 3)))
 
     def test_value_error_if_promoted_piece_not_finishing_two_squares_away_diagonally(self):
         self.piece.promote()
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (1, 3), (4, 6)))
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (2, 6), (4, 6)))
-        self.assertRaises(ValueError, Capture.__init__, (self.piece, (5, 3), (3, 0)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (3, 1), (6, 4)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (6, 2), (6, 4)))
+        self.assertRaises(ValueError, Capture.__init__, (self.piece, (3, 5), (0, 3)))
 
     def test_no_value_error_if_unpromoted_piece_makes_a_valid_capture_movement(self):
         try:
-            Capture(self.piece, (5, 3), (3, 5))
-            Capture(self.piece, (2, 4), (4, 6))
+            Capture(self.piece, (3, 5), (5, 3))
+            Capture(self.piece, (4, 2), (6, 4))
         except ValueError:
             self.fail("Value error raised despite valid capture arguments")
 
     def test_no_value_error_if_promoted_piece_makes_a_valid_capture_movement(self):
         self.piece.promote()
         try:
-            Capture(self.piece, (6, 2), (4, 4))
-            Capture(self.piece, (4, 4), (6, 2))
-            Capture(self.piece, (5, 1), (3, 3))
+            Capture(self.piece, (2, 6), (4, 4))
+            Capture(self.piece, (4, 4), (2, 6))
+            Capture(self.piece, (1, 5), (3, 3))
             Capture(self.piece, (3, 3), (5, 1))
         except ValueError:
             self.fail("Value error raised despite valid capture arguments")
--- test/test_draughts/test_move.py
+++ test/test_draughts/test_move.py
@@ -11,38 +11,38 @@ class MoveTest(unittest.TestCase):
         self.piece = Piece(0)
 
     def test_value_error_if_start_position_not_on_board(self):
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (-1, 0), (0, 1)))
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (8, 0), (9, 1)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, -1), (1, 0)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 8), (1, 9)))
 
     def test_value_error_if_end_position_not_on_board(self):
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (-1, 1)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (1, -1)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (7, 7), (8, 8)))
 
     def test_value_error_if_both_positions_not_on_board(self):
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (-1, -5), (0, -6)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (-5, -1), (-6, 0)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (10, 10), (11, 11)))
 
     def test_value_error_if_start_position_not_on_dark_square(self):
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 1), (1, 2)))
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (3, 0), (4, 1)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (1, 0), (2, 1)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 3), (1, 4)))
 
     def test_value_error_if_unpromoted_piece_not_one_diagonal_forward(self):
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (0, 1)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (1, 0)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (0, 1)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (5, 5)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (2, 2), (1, 1)))
 
     def test_value_error_if_promoted_piece_not_one_diagonal_forward_or_back(self):
         self.piece.promote()
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (1, 0)))
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (1, 1), (1, 2)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (0, 0), (0, 1)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (1, 1), (2, 1)))
         self.assertRaises(ValueError, Move.__init__, (self.piece, (2, 2), (0, 0)))
-        self.assertRaises(ValueError, Move.__init__, (self.piece, (3, 5), (1, 7)))
+        self.assertRaises(ValueError, Move.__init__, (self.piece, (5, 3), (7, 1)))
 
     def test_no_value_errors_if_valid_move_unpromoted(self):
         try:
             Move(self.piece, (0, 0), (1, 1))
-            Move(self.piece, (3, 5), (2, 6))
+            Move(self.piece, (5, 3), (6, 2))
             Move(self.piece, (5, 5), (6, 6))
         except ValueError:
             self.fail("Value error raised despite valid move arguments.")
@@ -51,11 +51,11 @@ class MoveTest(unittest.TestCase):
         self.piece.promote()
         try:
             Move(self.piece, (0, 0), (1, 1))
-            Move(self.piece, (3, 5), (2, 4))
-            Move(self.piece, (2, 2), (1, 3))
-            Move(self.piece, (5, 5), (6, 4))
+            Move(self.piece, (5, 3), (4, 2))
+            Move(self.piece, (2, 2), (3, 1))
+            Move(self.piece, (5, 5), (4, 6))
         except ValueError:
-            self.fail("Value error raised despite valid movee arguments.")
+            self.fail("Value error raised despite valid move arguments.")
 
 
 if __name__ == '__main__':
