f3fa14e4d4e0f201332c57fb715fb114e83fc7b0
jackoc1 <jackaoconnor1@gmail.com>
2022-01-12 00:21:04 +0000

    Added type hints and create custom board method

--- draughts/draughts.py
+++ draughts/draughts.py
@@ -1,8 +1,10 @@
+from typing import Tuple, List
+
 from draughts.board import Board
 
 
 class Draughts:
-    def __init__(self, player_1, player_2):
+    def __init__(self, player_1, player_2) -> "Draughts":
         self._board = Board()
         self._player_1 = player_1  # import different classes based on command line arguments
         self._player_2 = player_2
@@ -12,19 +14,20 @@ class Draughts:
     player_2 = property(lambda self: self._player_2)
     turn_count = property(lambda self: self._turn_count)
 
-    def get_board(self): return
+    def get_board(self) -> None: return
 
-    def new_game(self, swap_pieces): return
-
-    @staticmethod
-    def valid_moves(board): return
-
-    def _next_turn(self): return
+    def new_game(self, swap_pieces: bool) -> None: return
 
     @staticmethod
-    def _valid_moves(board, turn_count): return
+    def valid_moves(board: Board) -> Tuple[((int, int), (int, int))]: return
 
     @staticmethod
-    def _valid_captures(board, turn_count): return
+    def create_custom_board(sample_board: List[List[int]]) -> "Board": return
 
+    def _next_turn(self) -> None: return
 
+    @staticmethod
+    def _valid_moves(board: Board, turn_count: int) -> Tuple[((int, int), (int, int))]: return
+
+    @staticmethod
+    def _valid_captures(board: Board, turn_count: int) -> Tuple[((int, int), (int, int))]: return
