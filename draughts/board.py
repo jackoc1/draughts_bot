26cc2a08d7090f770aeb273da009ea62ed17a4f8
jackoc1 <jackaoconnor1@gmail.com>
2022-01-12 00:25:00 +0000

    Type hints added for Board

--- draughts/board.py
+++ draughts/board.py
@@ -1,5 +1,10 @@
+from typing import Tuple
+
+from draughts.player import AbstractPlayer
+
+
 class Board:
-    def __init__(self, num_rows=8, num_cols=8):
+    def __init__(self, num_rows=8, num_cols=8) -> "Board":
         self._board = [[0] * num_rows] * num_cols
         self._num_rows = num_rows
         self._num_cols = num_cols
@@ -7,38 +12,38 @@ class Board:
     num_rows = property(lambda self: self._num_rows)
     num_cols = property(lambda self: self._num_cols)
 
-    def move(self, start_position, end_position):
+    def move(self, start_position: (int, int), end_position: (int, int)) -> None:
         pass
 
     def get_board(self) -> "Board":  # don't expose internals
         pass
 
-    def get_player_pieces(self, player):
+    def get_player_pieces(self, player: AbstractPlayer) -> Tuple[("Piece", (int, int))]:
         pass
 
-    def get_piece(self, position):
+    def get_piece(self, position: (int, int)) -> "Piece":
         pass
 
-    def promote(self, piece):
+    def promote(self, piece: "Piece") -> None:
         pass
 
-    def add_piece(self, position):
+    def add_piece(self, position: (int, int)) -> None:
         pass
 
-    def remove_piece(self, position):
+    def remove_piece(self, position: (int, int)) -> None:
         pass
 
 
 class Piece:
-    def __init__(self, player):
+    def __init__(self, player: AbstractPlayer) -> "Piece":
         self._player = player
         self._promoted = False
 
     player = property(lambda self: self._player)
     promoted = property(lambda self: self._promoted)
 
-    def promote(self):
+    def promote(self) -> None:
         pass
 
-    def __eq__(self, other_piece):
+    def __eq__(self, other_piece: "Piece") -> bool:
         pass
