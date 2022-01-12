7c3fcd4f50a0a3702dc36b2233c94fd1360eb2ea
jackoc1 <jackaoconnor1@gmail.com>
2022-01-12 14:44:30 +0000

    Unit tests for equality checking Pieces

--- test/test_draughts/test_piece.py
+++ test/test_draughts/test_piece.py
@@ -32,10 +32,33 @@ class PieceTest(unittest.TestCase):
         self.assertEqual(piece.promoted, True)
 
     def test_equal_pieces_are_equal(self):
-        pass
+        piece_1 = Piece(0)
+        test_piece_1 = Piece(0)
+        piece_2 = Piece(1)
+        test_piece_2 = Piece(1)
+        piece_3 = Piece(0)
+        piece_3.promote()
+        test_piece_3 = Piece(0)
+        test_piece_3.promote()
+
+        self.assertEqual(piece_1, test_piece_1)
+        self.assertEqual(piece_2, test_piece_2)
+        self.assertEqual(piece_3, test_piece_3)
 
     def test_not_equal_pieces_are_not_equal(self):
-        pass
+        piece_1 = Piece(0)
+        test_piece_1 = Piece(1)
+        piece_2 = Piece(0)
+        piece_2.promote()
+        test_piece_2 = Piece(0)
+        piece_3 = Piece(0)
+        test_piece_3 = Piece(1)
+        piece_3.promote()
+        test_piece_3.promote()
+
+        self.assertNotEqual(piece_1, test_piece_1)
+        self.assertNotEqual(piece_2, test_piece_2)
+        self.assertNotEqual(piece_3, test_piece_3)
 
 
 if __name__ == '__main__':
