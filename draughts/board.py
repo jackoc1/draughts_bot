bb0bc848d3e64f7c97859e1c0d0f59776e463c8a
jackoc1 <jackaoconnor1@gmail.com>
2022-01-12 18:51:52 +0000

    __eq__ added to Board and method signature changes

--- draughts/board.py
+++ draughts/board.py
@@ -1,6 +1,4 @@
-from typing import Tuple, Type
-
-from draughts.player import AbstractPlayer
+from typing import Tuple
 
 
 class Board:
@@ -18,28 +16,42 @@ class Board:
     def get_board(self) -> "Board":  # don't expose internals
         pass
 
-    def get_player_pieces(self, player: Type[AbstractPlayer]) -> Tuple[("Piece", (int, int))]:
+    def get_player_pieces(self, colour: int) -> Tuple[("Piece", (int, int))]:
         pass
 
     def get_piece(self, position: Tuple[int, int]) -> "Piece":
         pass
 
-    def promote(self, piece: "Piece") -> None:
+    def promote(self, position: Tuple[int, int]) -> None:
         pass
 
-    def add_piece(self, position: Tuple[int, int]) -> None:
+    def add_piece(self, colour: int, position: Tuple[int, int]) -> None:
         pass
 
     def remove_piece(self, position: Tuple[int, int]) -> None:
         pass
 
+    def __equal__(self, other: "Board") -> bool:
+        equal = True
+        if self.num_rows != other.num_rows:
+            equal = False
+        elif self.num_cols != other.num_cols:
+            equal = False
+        else:
+            for row in range(self.num_rows):
+                for col in range(self.num_cols):
+                    if self.get_piece((row, col)) != other.get_piece((row, col)):
+                        equal = False
+                        break
+        return equal
+
 
 class Piece:
-    def __init__(self, player: AbstractPlayer) -> None:
-        self._player = player
+    def __init__(self, colour: int) -> None:
+        self._colour = colour
         self._promoted = False
 
-    player = property(lambda self: self._player)
+    colour = property(lambda self: self._colour)
     promoted = property(lambda self: self._promoted)
 
     def promote(self) -> None:
