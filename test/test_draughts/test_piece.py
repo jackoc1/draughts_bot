41c93eaea4808b9292e68ffeac6606a59d504717
jackoc1 <jackaoconnor1@gmail.com>
2022-01-12 18:05:40 +0000

    Player changed to colour

--- test/test_draughts/test_piece.py
+++ test/test_draughts/test_piece.py
@@ -7,24 +7,24 @@ class PieceTest(unittest.TestCase):
     def test_no_playerless_instantiation(self):
         self.assertRaises(Exception, Piece.__init__, ())
 
-    def test_player_must_be_0_or_1(self):
-        Piece(0)
-        Piece(1)
+    def test_colour_must_be_0_or_1(self):
+        Piece(colour=0)
+        Piece(colour=1)
         self.assertRaises(Exception, Piece.__init__, 2)
         self.assertRaises(Exception, Piece.__init__, "dark")
 
     def test_default_promoted_status_false(self):
-        piece = Piece(0)
+        piece = Piece(colour=0)
         self.assertEqual(piece.promoted, False)
 
     def test_promote_unpromoted_piece(self):
-        piece = Piece(1)
+        piece = Piece(colour=1)
         self.assertEqual(piece.promoted, False)
         piece.promote()
         self.assertEqual(piece.promoted, True)
 
     def test_promote_promoted_piece(self):
-        piece = Piece(0)
+        piece = Piece(colour=0)
         self.assertEqual(piece.promoted, False)
         piece.promote()
         self.assertEqual(piece.promoted, True)
@@ -32,13 +32,13 @@ class PieceTest(unittest.TestCase):
         self.assertEqual(piece.promoted, True)
 
     def test_equal_pieces_are_equal(self):
-        piece_1 = Piece(0)
-        test_piece_1 = Piece(0)
-        piece_2 = Piece(1)
-        test_piece_2 = Piece(1)
-        piece_3 = Piece(0)
+        piece_1 = Piece(colour=0)
+        test_piece_1 = Piece(colour=0)
+        piece_2 = Piece(colour=1)
+        test_piece_2 = Piece(colour=1)
+        piece_3 = Piece(colour=0)
         piece_3.promote()
-        test_piece_3 = Piece(0)
+        test_piece_3 = Piece(colour=0)
         test_piece_3.promote()
 
         self.assertEqual(piece_1, test_piece_1)
@@ -46,13 +46,13 @@ class PieceTest(unittest.TestCase):
         self.assertEqual(piece_3, test_piece_3)
 
     def test_not_equal_pieces_are_not_equal(self):
-        piece_1 = Piece(0)
-        test_piece_1 = Piece(1)
-        piece_2 = Piece(0)
+        piece_1 = Piece(colour=0)
+        test_piece_1 = Piece(colour=1)
+        piece_2 = Piece(colour=0)
         piece_2.promote()
-        test_piece_2 = Piece(0)
-        piece_3 = Piece(0)
-        test_piece_3 = Piece(1)
+        test_piece_2 = Piece(colour=0)
+        piece_3 = Piece(colour=0)
+        test_piece_3 = Piece(colour=1)
         piece_3.promote()
         test_piece_3.promote()
 
